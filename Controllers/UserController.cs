d40756e37862a4e38719fb5d28760c3da8630d52
adithravindran0fficial <adithravindranofficial@gmail.com>
2024-10-20 23:27:13 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionFilters.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public List<USers> users;
+        public UserController() {
+             users = new List<USers> { new USers { Name="adith",Email="adithcom"},
+                new USers{Name="Anandhu",Email="Anandhu.com"},
+                new USers{Name="Nandhu",Email="Nandhu.com"} };
+        }
+        [HttpGet]
+        [Logging]
+        public IActionResult GetAll()
+        {
+            return Ok(users);
+        }
+    }
+    public class USers
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+       
+            
+    }  
+}
---
+++ Logging.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ActionFilters
+{
+    public class Logging : Attribute, IActionFilter
+    {
+        void IActionFilter.OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("this is After");
+        }
+
+        void IActionFilter.OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("this is before");
+            
+        }
+    }
+}
