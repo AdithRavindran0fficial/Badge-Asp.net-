7e7212c9eca7ada3d10dae6228e95f2838c03989
adithravindran0fficial <adithravindranofficial@gmail.com>
2024-10-21 10:05:56 +0530

    calculating time to execute a controller action using action filter

--- Logging.cs
+++ Logging.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
 
 namespace ActionFilters
 {
     public class Logging : Attribute, IActionFilter
     {
+        private Stopwatch stopWatch;
         void IActionFilter.OnActionExecuted(ActionExecutedContext context)
         {
-            Console.WriteLine("this is After");
+            stopWatch.Stop();
+            var elapsedmilisec = stopWatch.ElapsedMilliseconds;
+            
+            Console.WriteLine($"this method takes {elapsedmilisec} to execute");
         }
 
         void IActionFilter.OnActionExecuting(ActionExecutingContext context)
         {
-            Console.WriteLine("this is before");
+            stopWatch.Start();
+            Console.WriteLine("this is before and stop watch started");
             
         }
     }
